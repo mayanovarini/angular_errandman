b2a854239cc89189a37689f8d6b57d6faa612a00
mayanovarini@gmail.com <mayanovarini@gmail.com>
2016-06-29 16:02:32 -0700

    first commit

---
+++ scripts/app.js
@@ -0,0 +1,42 @@
+'use strict';
+
+var app = angular
+  .module('ErrandMan', [
+    'ngAnimate',
+    'ngResource',
+    'ngRoute',
+    'firebase',
+    'toaster'
+  ])
+  .constant('FURL', 'https://errandman.firebaseio.com/')
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html'
+      })
+      .when('/login', {
+        templateUrl: 'views/login.html',
+        controller: 'AuthController'
+      })
+      .when('/register', {
+        templateUrl: 'views/register.html',
+        controller: 'AuthController'
+      })
+      .when('/post', {
+        templateUrl: 'views/post.html',
+        controller: 'TaskController'
+      })
+      .when('/edit/:taskId', {
+        templateUrl: 'views/edit.html',
+        controller: 'TaskController'
+      })
+      .when('/browse', {
+        templateUrl: 'views/browse.html',
+        controller: 'TaskController'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+
+
+  });
